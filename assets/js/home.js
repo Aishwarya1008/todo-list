6401538f4a58a8a0bc36e765706c6dcdf83c10b0
Aishwarya <aishwarya.rohatgi1008@gmail.com>
2021-04-01 01:43:48 +0530

    Create todo app

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+// var dates = $('#todo-details span');
+// for(let i = 0; i<dates.length; i++){
+//     var date = dates.eq(i).text();
+//     console.log(date);
+//     var longDate = new Date(date);
+//     console.log(longDate);
+// }
+
--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 const express = require('express');
 const port = 8000;
 
+const db = require('./config/mongoose');
+const Todos = require('./models/todos');
+var ObjectId = require('mongodb').ObjectID;
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static('assets'));
+app.use(express.urlencoded());
+
+// const todos = [
+//     {
+//         description: 'Why not add a task',
+//         category: 'Work',
+//         date: '2021-03-31'
+//     },
+//     {
+//         description: 'Why not add a task',
+//         category: 'Work',
+//         date: '2021-03-31'
+//     }
+// ];
 
 app.get('/', function(req, res){
-    res.render('home', {
-        title: 'Todo-List'
+    Todos.find({}, function(err, todos){
+        if(err){
+            console.log('Error in fetching', err);
+            return;
+        }
+        return res.render('home', {
+            title: 'Todo-List',
+            todo_list: todos
+        });
+
     });
+    
 });
 
+app.post('/add-todo', function(req, res){
+    // console.log(req.body);
+    Todos.create(req.body);
+    res.redirect('back');
+});
+
+app.post('/delete-todos', function(req, res){
+    var ids = req.body.task;
+    if(!ids){
+        return res.redirect('back');
+    }
+    if(typeof(ids) == "string"){
+        Todos.findByIdAndDelete(ids, function(err){
+            if(err){
+                console.log('error in deleting', err);
+                return;
+            }
+            return res.redirect('back');
+        });
+    } else {
+        ids.forEach(element => {
+            Todos.findByIdAndDelete(element, function(err){
+                if(err){
+                    console.log('error in deleting', err);
+                    return;
+                }
+            });
+        });
+        return res.redirect('back');
+    }
+    
+});
 
 app.listen(port, function(err){
     if(err){
